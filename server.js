a324ea944e2b18e1ee510a612e69b61385660048
Kumar Atul <atultunnu2@gmail.com>
2020-05-12 19:18:34 +0530

    "ids are not indexes- improved using some brain"

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.post('/addTennisPlayer',(req,res)=>{
 })
 app.post('/updateTennisPlayer',(req,res)=>{
     for(let player of players_data){
-        console.log(player.id,req.body.id)
-        if (player.id==req.body.id){
+        // console.log(player.id,req.body.id)
+        if (parseInt( player.id)==parseInt(req.body.id)){
             player.name=req.body.name
             player.country=req.body.country
             break
         }
-    }
+    };
     res.redirect('/')
 });
 app.get('/',(req,res)=>{
@@ -37,22 +37,25 @@ app.get('/',(req,res)=>{
 })
 // app.get('/',(req,res)=>res.send("sent right"));
 app.get('/player_del/:id',(req,res)=>{
-    let this_player_id;
-    for (let player of players_data){
-        if (player.id==req.params.id){
-            this_player_id=player
+    let this_player_indx;
+    for (this_player_indx in players_data){
+        // console.log(players_data[this_player_indx],req.params.id)
+        if (parseInt( players_data[this_player_indx].id)==parseInt( req.params.id)){
+            break
         }
     }
-    players_data.splice(this_player_id,1);
+    players_data.splice(this_player_indx,1);
     res.redirect('/')
 })
 app.get('/player_edit/:id',(req,res)=>{
-    let this_player;
-    for (let player of players_data){
-        if (player.id==req.params.id){
-            this_player=player
+    let this_player_indx;
+    for (this_player_indx in players_data){
+        // console.log(players_data[this_player_indx],req.params.id)
+        if (parseInt( players_data[this_player_indx].id)==parseInt( req.params.id)){
+            break
         }
     }
-    res.render('form_update',{this_player})
+    let player=players_data[this_player_indx]
+    res.render('form_update',{player})
 });
 app.listen(process.env.PORT || 5000,console.log("started"));
